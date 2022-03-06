6595893790c61d1f415ae4d9a6dc981e19b7d90a
RARES129 <94977726+RARES129@users.noreply.github.com>
2022-03-06 04:39:05 +0200

    Delete global.h

--- Laborator 2/ex 2/global.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "class1.h"
-
-int comparareNume(class1* f1, class1* f2);
-int comparareMatematica(class1* f1, class1* f2);
-int comparareEngleza(class1* f1, class1* f2);
-int comparareIstorie(class1* f1, class1* f2);
-int comparareMedie(class1* f1, class1* f2);
-
